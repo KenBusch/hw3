7f9e9fac543af917ba900fcd13c901319d54214a
KenBusch <ken0505@gmail.com>
2021-04-21 16:27:32 -0500

    Version 1.0

--- noober.js
+++ noober.js
@@ -11,7 +11,54 @@ document.addEventListener('DOMContentLoaded', async function() {
   console.log(ride)
 
   // 🔥 YOUR CODE GOES HERE 🔥
-  // Write the recipe (algorithm) in the comments. Then, write the code.
+  // Write the recipe (algorithm) in the comments. Then, write the code
   
+  //Declare variable for the Rider First Name
+  let passengerFirstName = ride.passengerDetails.first
+
+  //Declare variable for the Rider First Name
+  let passengerLastName = ride.passengerDetails.last
+
+  //Declare variable for the Phone Number
+  let passengerphoneNumber = ride.passengerDetails.phoneNumber
+
+  //Declare variable for pickup street address
+  let passengerPickupAdrress = ride.pickupLocation.address
+
+  //Declare variable for pickup city
+  let passengerPickupCity = ride.pickupLocation.city
+
+  //Declare variable for pickup state
+  let passengerPickupState = ride.pickupLocation.state
+
+  //Declare variable for pickup zip
+  let passengerPickupZip = ride.pickupLocation.zip
+
+  //Declare variable for Dropoff street address
+  let passengerDropoffAdrress = ride.dropoffLocation.address
+
+  //Declare variable for Dropoff city
+  let passengerDropoffCity = ride.dropoffLocation.city
+
+  //Declare variable for Dropoff state
+  let passengerDropoffState = ride.dropoffLocation.state
+
+  //Declare variable for Dropoff zip
+  let passengerDropoffZip = ride.dropoffLocation.zip
+
+  //Declare Variable for level of service using conditional logic
+  let serviceLevel
+
+  if (ride.purpleRequested == true) {
+    serviceLevel = `Purple`
+  } else if (ride.numberOfPassengers > 3) {
+    serviceLevel = `XL`
+  } else {
+    serviceLevel = `X`
+  }
+
+//Display passenger info in a readable format using variables
+console.log(`Noober ${serviceLevel} Passenger: ${passengerFirstName} ${passengerLastName} - ${passengerphoneNumber}. Pickup at ${passengerPickupAdrress}, ${passengerPickupCity}, ${passengerPickupState} ${passengerPickupZip}. Drop-off at ${passengerDropoffAdrress}, ${passengerDropoffCity}, ${passengerDropoffState} ${passengerDropoffZip}`)
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
